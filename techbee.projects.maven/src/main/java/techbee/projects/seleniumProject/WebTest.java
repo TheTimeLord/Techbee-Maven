07beb4da409b04480e99e254424a1b4bb5e61ff9
TheTimeLord <brianandrus2@gmail.com>
2021-04-15 15:31:44 -0700

    I got only part of the Target scraping to work, the first 6 items are shown

--- techbee.projects.maven/src/main/java/techbee/projects/seleniumProject/WebTest.java
+++ techbee.projects.maven/src/main/java/techbee/projects/seleniumProject/WebTest.java
@@ -46,32 +46,15 @@ public class WebTest {
 		searchButton = chrome.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[2]/nav/div/form/button[2]"));
 		searchButton.click();
 		Thread.sleep(2000);
-		/*
-		//																		 \/
-		///html/body/div[1]/div/div[4]/div[4]/div[2]/div/div[1]/div[3]/div/ul/li[1]/div/div[2]/div/div/div/div[1]/div[1]/a Link-sc-1khjl8b-0
-		///html/body/div[1]/div/div[4]/div[4]/div[2]/div/div[1]/div[3]/div/ul/li[2]/div/div[2]/div/div/div/div[1]/div[1]/a Link-sc-1khjl8b-0
-		///html/body/div[1]/div/div[4]/div[4]/div[2]/div/div[1]/div[3]/div/ul/li[8]/div/div/div[2]/div/div/div/div[1]/div[1]/a
-		///html/body/div[1]/div/div[4]/div[4]/div[2]/div/div[1]/div[3]/div/ul/li[9]/div/div/div[2]/div/div/div/div[1]/div[1]/a
 		
-		//class="Link-sc-1khjl8b-0 styles__StyledTitleLink-mkgs8k-5 kdCHb inccCG h-display-block h-text-bold h-text-bs flex-grow-one"
-		//class="Link-sc-1khjl8b-0 styles__StyledTitleLink-mkgs8k-5 kdCHb inccCG h-display-block h-text-bold h-text-bs flex-grow-one"
-		// get new list of names and prices of iphones
-//		names = chrome.findElements(By.xpath("/html/body/div[1]/div/div[4]/div[4]/div[2]/div/div[1]/div[3]/div/ul/li"));
-		List<WebElement> elems = chrome.findElements(By.xpath("//div/div[2]/div/div/div/div[1]/div[1]/a"));
+		//NOTE: Target website has inconsistencies, I can only get the first 6 items from the page easily
+		names = chrome.findElements(By.xpath("//div/div[2]/div/div/div/div[1]/div[1]/a"));
+		prices = chrome.findElements(By.xpath("//div/div[2]/div/div/div/div[2]/div/div/span"));
 		
-		for(WebElement e: elems) {
-			String test = e.getText();
-			System.out.println(test);
-			
-		}
-		
-		//names = chrome.findElements(By.className("Row-uds8za-0"));
-		System.out.println(elems.size());
-		
-		for(WebElement e : names) {
-			//System.out.println(e.getText());
-		}
-		*/
+		if(names.size() != prices.size())
+			System.out.println("Error, number of names and prices do not match");
+		else
+			writeToFile(names, prices, "Target.txt");	
 	}
 	
 	private static void writeToFile(List<WebElement> names, List<WebElement> prices, String filename) {
@@ -98,6 +81,7 @@ public class WebTest {
 			for(int i=0; i < names.size(); i++) {
 				String txt = names.get(i).getText() + " - " + prices.get(i).getText() + "\n";
 				writer.write(txt);
+				System.out.print(txt);
 			}
 			writer.close();
 			
